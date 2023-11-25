1bbb344ba09c2e02eb1a5b30eb8a151f3721ba08
gbehsuw <131389823+gbehsuw@users.noreply.github.com>
2023-11-24 19:22:53 -0500

    Add files via upload

---
+++ questions.py
@@ -0,0 +1,330 @@
+def realSubjects():
+  realSubjects = ["MATH", "SCIENCE", "COMPSCI"]
+  return realSubjects
+
+
+def subjectCategories():
+  subjectCategories = [
+    # MATH QUESTIONS
+    [
+      [
+        { 
+          "question": 'Given 2+2=x, find x',
+          "answer": '4'
+        }
+      ],
+      [
+        { 
+          "question": '2+2=x; What is x?',
+          "answer": '4'
+        }
+      ],
+      [
+        { 
+          "question": '5^0',
+          "answer": '1'
+        }
+      ],
+      [
+        { 
+          "question": 'Factor x^2-14x-15 fully with no negative exponents in the result.',
+          "answer": '(x-7)^2'
+        }
+      ],
+      [
+        { 
+          "question": 'Fully factor 3x^4y^5-12x^3y^4-63x^2y^3 with no negative exponents in the final answer.',
+          "answer": '3x^2y^3(xy-7)(xy+3)'
+        }
+      ],
+      [
+        { 
+          "question": 'The slope of a line through the points A(-1,1) and B(4,3)',
+          "answer": '2/5'
+        }
+      ],
+      [
+        { 
+          "question": 'A rock is dropped into a pond sends out a circular ripple whose radius increases steadily at 25cm/s. A toy boat is floating on the pond 4m east and 3m north of the spot where the rock is dropped. How many seconds does it take for the ripple to reach the toy boat?',
+          "answer": '20'
+        }
+      ],
+      [
+        { 
+          "question": 'The equation of a line that passes through (5,-3) and is parallel to the y-axis.',
+          "answer": 'x=5'
+        }
+      ],
+      [
+        { 
+          "question": 'Fully factor 10x^2y^-1-15xy^3 leaving no negative exponents.',
+          "answer": '5x(2x-3y^4)/y'
+        }
+      ],
+      [
+        { 
+          "question": 'A rectangle with positive integer side lengths in cm has are A cm^2 and perimeter P cm. Which of the following numbers cannot equal A+P? 100, 102, 104, 106 or 108?',
+          "answer": '102'
+        }
+      ],
+      [
+        { 
+          "question": 'Factorize (x^2-3x+2)^2-30x^2+90x-60 fully and leave no negative exponents in the answer.',
+          "answer": '(x+4)(x-7)(x-1)(x-2)'
+        }
+      ],
+      [
+        { 
+          "question": 'A boys & girls club found that it  could achieve a membership ratio of 2 girls to one boy either by inducting 24 girls or by expelling x boys. What is x?',
+          "answer": '12'
+        }
+      ],
+      [
+        { 
+          "question": 'A box contains a collection of triangular and square tiles. There are 25 tiles in the box, containing 84 edges total. How many square tiles are there in the box?',
+          "answer": '9'
+        }
+      ],
+      [
+        { 
+          "question": 'What is the 11th term of the sequence a = 2n-6?',
+          "answer": '16'
+        }
+      ],
+      [
+        { 
+          "question": 'What is the sum of the first six terms of the sequence a = 2n+6?',
+          "answer": '78'
+        }
+      ],
+      [
+        { 
+          "question": 'What is the common ratio r in the geometric sequence {0.5, 1.5, 4.5, 13.5, 40.5,...}',
+          "answer": '3'
+        }
+      ],
+      [
+        { 
+          "question": 'For each positive integer n, the mean of the first n terms of a sequence is n. What is the 2008th term of the sequence?',
+          "answer": '4015'
+        }
+      ],
+      [
+        { 
+          "question": 'Justin Shi divides a circle into 12 sectors. The central angles of these sectors, measured in degrees, are all integers and they form an arithmetic sequence. What is the degree measure of the smallest possible sector angle?',
+          "answer": '8'
+        }
+      ],
+      [
+        { 
+          "question": 'Mr.Patrick teaches math to 15 students. He was grading tests and found that when he graded everyones test except Paytons, the average grade for the class was 80. After he graded Paytons test, the test average became 81. What was Paytons score on the test?',
+          "answer": '95'
+        }
+      ],
+      [
+        { 
+          "question": 'Given the circle (x-3)^2+(y+1)^2=16, what is the radius?',
+          "answer": '4'
+        }
+      ],
+      [
+        { 
+          "question": 'The sum of two positive numbers is 5 times that of their difference. What is the ratio of the larger number to the smaller number?',
+          "answer": '3/2'
+        }
+      ],
+      [
+        { 
+          "question": 'How many rearrangements of abcd are there in which no two adjacent letters in the alphabet? For example, no such rearrangements could include either ab or ba.',
+          "answer": '2'
+        }
+      ],
+      [
+        { 
+          "question": 'How many non-similar triangles have angles whose degree measures are distinct positive integers in arithmetic progression?',
+          "answer": '59'
+        }
+      ],
+      [
+        { 
+          "question": 'Claudia has 12 coins, each of which is a 5 cent coin or a 10 cent coin. There are exactly 17 different values that can be obtained as combinations of one or more of her coins. How many 10 cent coins does Claudia have?',
+          "answer": '5'
+        }
+      ],
+      [
+        { 
+          "question": 'In the sequence 2001, 2002, 2003, ..., each term after the third is found by subtracting the previous term from the sum of the two terms that precede that term. For example, the fourth term is 2001+2002-2003 = 2000. What is the 2004th term in this sequence?',
+          "answer": '0'
+        }
+      ],
+      [
+        { 
+          "question": 'The first term of a sequence is 2005. Each succeeding term is the sum of the cubes of the digits of the previous term. What is the 2005th term of the sequence?',
+          "answer": '250'
+        }
+      ],
+    ], 
+    # SCIENCE QUESTIONS
+    [
+      [
+        {
+          "question": 'What is the name for MgO? (ANSWER ALL IN CAPITALS)',
+          "answer": 'MAGNESIUM OXIDE'
+        }
+      ],
+      [
+        {
+          "question": 'What is the name for H2CO3(aq)? (ANSWER ALL IN CAPITALS)',
+          "answer": 'CARBONIC ACID'
+        }
+      ],
+      [
+        {
+          "question": 'What is the name for LiCH3COO? (ANSWER ALL IN CAPITALS)',
+          "answer": 'LITHIUM ACETATE'
+        }
+      ],
+      [
+        {
+          "question": 'What are the possible reactants for the following result? \nSrSO4(aq)+H2O(l)',
+          "answer": 'Sr(OH)2(aq)+H2SO4(aq)'
+        }
+      ],
+      [
+        { 
+          "question": 'Predict the product for the reaction: Cr(s)+SnCI4(aq)',
+          "answer": 'Sn(s)+CrCI2(aq)'
+        }
+      ],
+      [
+        { 
+          "question": 'What is the correct name for the compound P4S9? (answer in all caps) (ANSWER ALL IN CAPITALS)',
+          "answer": 'TETRAPHOSPHOURUS NONASULFILE'
+        }
+      ],
+      [
+        { 
+          "question": 'When testing for carbon dioxide, is a glowing or flaming splint used? (ANSWER ALL IN CAPITALS)',
+          "answer": 'FLAMING'
+        }
+      ],
+      [
+        { 
+          "question": 'What organelle breaks down & recycles old organelles and only occurs in animal cells? (ANSWER ALL IN CAPITALS)',
+          "answer": 'LYSOSOME'
+        }
+      ],
+      [
+        { 
+          "question": 'What is the \'powerhouse\' of the cell? (ANSWER ALL IN)',
+          "answer": 'MITOCHONDRIA'
+        }
+      ],
+      [
+        { 
+          "question": 'Determine whether burning wood is a PHYSICAL CHANGE(PC) or a CHEMICAL CHANGE(CC)',
+          "answer": 'CC'
+        }
+      ],
+      [
+        { 
+          "question": 'Determine whether water freezing to ice is a PHYSICAL CHANGE(PC) or a CHEMICAL CHANGE(CC)',
+          "answer": 'PC'
+        }
+      ],
+      [
+        { 
+          "question": 'Determine whether cutting a tomato is a PHYSICAL CHANGE(PC) or a CHEMICAL CHANGE(CC)',
+          "answer": 'PC'
+        }
+      ],
+      [
+        { 
+          "question": 'Determine whether sugar dissolving in water is a PHYSICAL CHANGE(PC) or a CHEMICAL CHANGE(CC)',
+          "answer": 'PC'
+        }
+      ],
+      [
+        { 
+          "question": 'Determine whether silver spoons changing colour when reacting with oxygen is a PHYSICAL CHANGE(PC) or a CHEMICAL CHANGE(CC)',
+          "answer": 'CC'
+        }
+      ],
+      [
+        { 
+          "question": 'Muddy water is a heterogeneous mixture. (Answer TRUE or FALSE)',
+          "answer": 'TRUE'
+        }
+      ],
+      [
+        { 
+          "question": 'What is the second phase of mitosis? (ANSWER IN ALL CAPS)',
+          "answer": 'METAPHASE'
+        }
+      ],
+      [
+        { 
+          "question": 'What is the fourth phase of mitosis? (ANSWER IN ALL CAPS)',
+          "answer": 'TELOPHASE'
+        }
+      ],
+      [
+        { 
+          "question": 'Protons have a charge of 2 (Answer TRUE or FALSE)',
+          "answer": 'FALSE'
+        }
+      ],
+    ],
+    ### COMPSCI QUESTIONS AND ANSWERS
+    [
+      [
+        { 
+          "question": 'What number system do computers "think" in (respond in all caps)',
+          "answer": 'BINARY'
+        }
+      ],
+      [
+        { 
+          "question": 'What does 101 in binary equate to in decimal?',
+          "answer": '5'
+        }
+      ],
+      [
+        { 
+          "question": 'Matthew wants to use a flashlight to communicate in morsecode to a friend across the classroom to cheat on a test. Unfortunately for Matthew, his flashlight only has 2 states (on,off) while morse code requires 3 (dit,dat,empty space). Matthew decides to snap every time a new digit should appear on the flashlight so his friend knows when he is changing between dits or dats. What does this solution directly mirror within computers? (Answer in all caps)',
+          "answer": 'INTERNAL CLOCK'
+        }
+      ],
+      [
+        { 
+          "question": 'What does 1111 in binary equate to in decimal?',
+          "answer": '15'
+        }
+      ],
+      [
+        { 
+          "question": 'Which symbols do I use to put a cin into a variable for c++?',
+          "answer": '>>'
+        }
+      ],
+      [
+        { 
+          "question": 'How do you include the iostream library in c++?',
+          "answer": '#include <iostream>'
+        }
+      ],
+      [
+        { 
+          "question": 'What is the logical operator for not in conditons in c++?',
+          "answer": '!='
+        }
+      ],
+      [
+        { 
+          "question": 'Based on the method signature \'public static double division(int returnNumber, float output)\', what is the return type?',
+          "answer": 'double'
+        }
+      ],
+    ]
+  ]
+  return subjectCategories
